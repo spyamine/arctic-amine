9c628b9d7bfd367a173e2497bdfa4d89c94d1431
Mohamed Amine Guessous <guessous.amine@gmail.comm>
2022-12-31 15:36:06 +0100

    adding numpy as requirement for the setup

--- setup.py
+++ setup.py
@@ -69,7 +69,8 @@ setup(
     long_description='\n'.join((long_description, changelog)),
     long_description_content_type="text/markdown",
     cmdclass={'test': PyTest},
-    setup_requires=["numpy<1.19.0",
+    setup_requires=[
+                    "numpy",
                     "setuptools-git",
                    ],
     install_requires=["decorator",
